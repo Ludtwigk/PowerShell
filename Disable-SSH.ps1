3f71db73b32069fe9504d7aa4d6aca5ab150186e
codewig <Ludwig.Beckert@outlook.de>
2022-05-25 10:29:23 +0200

    Disable SSH on all ESXi hosts of a vCenter

---
+++ Disable-SSH.ps1
@@ -0,0 +1,14 @@
+﻿param([string[]]$vCenter, [PSCredential]$Credential)
+
+$ErrorActionPreference = "Continue"
+$ConfirmPreference = "None"
+
+Import-Module -Name VMware.VimAutomation.Core
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip:$false -DefaultVIServerMode Multiple -Scope Session
+
+Connect-VIServer -Server $vCenter -Credential $Credential
+
+Get-VMHost | ForEach-Object { Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq "TSM-SSH"}) }
+
+Disconnect-VIServer -Server $vCenter
