7e4c3685e8a5d29170b678b1015a98624646da56
codewig <Ludwig.Beckert@outlook.de>
2022-11-10 14:23:40 +0100

    PowerShell Script to sort Pictures and Videos
    This script sorts Pictures and Videos based on the year they were taken and creats/moves into directories according to year and extension. 
    Example output structure:
    \2015
       \Images
       \Videos
    \2016
        \Images
        \Videos

---
+++ Sort-Pictures.ps1
@@ -0,0 +1,32 @@
+﻿param([String]$PathFrom, [String]$PathTo)
+
+$images = New-Object System.Collections.ArrayList
+$videos = New-Object System.Collections.ArrayList
+$years = New-Object System.Collections.ArrayList
+
+
+$all = Get-ChildItem -Path $PathFrom -Recurse | Sort-Object -Property LastWriteTime.Year
+
+$all | ForEach-Object { 
+    [void]$years.Add($_.LastWriteTime.Year)
+}
+
+$years = $years | Sort-Object -Unique
+
+$years | ForEach-Object {
+    $fp = $PathTo + "\" + $_
+    [System.IO.Directory]::CreateDirectory($fp)
+    [System.IO.Directory]::CreateDirectory($fp + "\Images")
+    [System.IO.Directory]::CreateDirectory($fp + "\Videos")
+}
+
+$all | ForEach-Object {
+    if ($_.Extension -iin ".mov",".mp4") {
+        $moveTo = $PathTo + "\" + $_.LastWriteTime.Year + "\Videos"
+        $_ | Move-Item -Destination $moveTo
+    } else {
+        $moveTo = $PathTo + "\" + $_.LastWriteTime.Year + "\Images"
+        $_ | Move-Item -Destination $moveTo
+    }
+}
+
